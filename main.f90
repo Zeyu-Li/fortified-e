d45ce798d0746ca472a874bff9f236c10444579f
Zeyu-Li <zeyu.li23@gmail.com>
2021-05-13 21:01:13 -0600

    started main

---
+++ main.f90
@@ -0,0 +1,15 @@
+program main
+
+    implicit none
+    double precision :: e = 0
+    integer :: iterations
+
+
+    print *, "Number of iterations: "
+    read *, iterations
+    
+
+    ! print e
+    print *, "e = ", huge(e)
+
+end program main
